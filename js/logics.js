b3d3972050fbfcc0bbd1514a7a26180e413e4a23
Roman <romansko90@gmail.com>
2018-02-10 01:07:52 +0200

    initial commit

---
+++ js/logics.js
@@ -0,0 +1,137 @@
+/// PE & PG Parser made by Roman Koifman (c) 2018.
+
+
+
+/************************** MAIN **************************/
+var cpuTypeSet, modeSet, pgSizeSet;
+var bX32, bX64;
+var bCompMode, bLongMode, bSysManagMode;
+var b4K, b2M, b1G;
+var PG;
+const HEX = 16, DEC = 10, BIN = 2;
+var prevLinearBase = HEX, prevPhysicalBase = HEX; 
+
+
+/***/
+
+/**
+ * On page loaded
+ */
+$(document).ready(function(){
+
+    // cpu set
+    cpuTypeSet = document.getElementById("cpuset");
+    bX32 = cpuTypeSet.elements[0];
+    bX32.onclick = x32Checked;
+    bX64 = cpuTypeSet.elements[1];
+    bX64.onclick = x64Checked;
+
+    // mode set
+    modeSet = document.getElementById("modeset");
+    bCompMode = modeSet.elements[0];
+    bLongMode = modeSet.elements[1];
+    bSysManagMode = modeSet.elements[2];
+
+    // paging related
+    PG = document.getElementById("pg");
+    pg.onclick = pgHandler;
+    pgSizeSet = document.getElementById("pgsize");
+    b4K = pgSizeSet.elements[0];
+    b2M = pgSizeSet.elements[1];
+    b1G = pgSizeSet.elements[2];
+
+    
+    // linear address
+    $("#linearAddressTextBox").keydown(function(e){
+        preventType(prevLinearBase, e);
+    });
+    $("#linearAddrrHex").change(function() {
+        $("#linearAddressTextBox").val(convertNumber($("#linearAddressTextBox").val(), prevLinearBase, 16));
+        prevLinearBase = HEX;
+    });
+    $("#linearAddrrDec").change(function() {
+        $("#linearAddressTextBox").val(convertNumber($("#linearAddressTextBox").val(), prevLinearBase, 10));
+        prevLinearBase = DEC;
+    });
+    $("#linearAddrrBin").change(function() {
+        $("#linearAddressTextBox").val(convertNumber($("#linearAddressTextBox").val(), prevLinearBase, 2));
+        prevLinearBase = BIN;
+    });
+
+    // physical address
+    $("#physicalAddressTextBox").keydown(function(e){
+        preventType(prevPhysicalBase, e);
+    });
+    $("#physicalAddrrHex").change(function() {
+        $("#physicalAddressTextBox").val(convertNumber($("#physicalAddressTextBox").val(), prevPhysicalBase, 16));
+        prevPhysicalBase = HEX;
+    });
+    $("#physicalAddrrDec").change(function() {
+        $("#physicalAddressTextBox").val(convertNumber($("#physicalAddressTextBox").val(), prevPhysicalBase, 10));
+        prevPhysicalBase = DEC;
+    });
+    $("#physicalAddrrBin").change(function() {
+        $("#physicalAddressTextBox").val(convertNumber($("#physicalAddressTextBox").val(), prevPhysicalBase, 2));
+        prevPhysicalBase = BIN;
+    });
+});
+
+
+function preventType(Base, e) {
+    if (Base === BIN) {
+        if (e.keyCode != 48 && e.keyCode != 49 && e.keyCode != 96 && e.keyCode != 97)
+            e.preventDefault();
+    }
+    if (Base === DEC) {
+        if (isNaN(String.fromCharCode(e.which)))
+            event.preventDefault(); 
+    }
+    if (Base === HEX) {
+        if (!((e.keyCode >= 48 && e.keyCode <= 70) || (e.keyCode >= 96 && e.keyCode <= 105)))
+            event.preventDefault(); 
+    }
+}
+
+/**
+ * convert number base
+ * @param {*} n num to convert
+ * @param {*} fromBase 
+ * @param {*} toBase 
+ */
+function convertNumber(n, fromBase, toBase) {
+    if (n === "") return 0;
+    return parseInt(n.toString(), fromBase).toString(toBase);
+  }
+
+function x32Checked() {
+    for (var i=0; i<modeSet.elements.length; ++i) {
+        modeSet.elements[i].disabled = true;
+        modeSet.elements[i].checked = false;
+    }
+    pgHandler();
+}
+
+function x64Checked() {
+    for (var i=0; i<modeSet.elements.length; ++i)
+        modeSet.elements[i].disabled = false;
+    bLongMode.checked = true;
+    pgHandler();
+}
+
+function pgHandler() {
+    if (PG.checked){
+        for (var i=0; i<pgSizeSet.elements.length; ++i)
+            pgSizeSet.elements[i].disabled = false;
+        b4K.checked = true;
+        if (bX32.checked){
+            b2M.disabled = true;
+            b1G.disabled = true;
+        }
+    }
+    else {
+        for (var i=0; i<pgSizeSet.elements.length; ++i) {
+            pgSizeSet.elements[i].disabled = true;
+            pgSizeSet.elements[i].checked = false;
+        }
+    }
+}
