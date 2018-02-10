e2f3603f5f18fc22a1f2d7b2d34a6203a07b86d2
Roman <romansko90@gmail.com>
2018-02-10 13:50:41 +0200

    updated stuff

--- js/logics.js
+++ js/logics.js
@@ -7,7 +7,7 @@ var cpuTypeSet, modeSet, pgSizeSet;
 var bX32, bX64;
 var bCompMode, bLongMode, bSysManagMode;
 var b4K, b2M, b1G;
-const HEX = 16, DEC = 10, BIN = 2;
+const HEX = 16, DEC = 10, BIN = 2, ERR = -1;
 var prevLinearBase = HEX, prevPhysicalBase = HEX; 
 
 
@@ -22,9 +22,9 @@ $(document).ready(function(){
     // cpu set
     cpuTypeSet = document.getElementById("cpuset");
     bX32 = cpuTypeSet.elements[0];
-    bX32.onclick = x32Checked;
     bX64 = cpuTypeSet.elements[1];
-    bX64.onclick = x64Checked;
+    $("#x32").change(x32Checked);
+    $("#x64").change(x64Checked);
 
     // mode set
     modeSet = document.getElementById("modeset");
@@ -75,49 +75,62 @@ $(document).ready(function(){
         prevPhysicalBase = BIN;
     });
 
-    $("#parseBtn").click(function(){
-        
+    $("#parseBtn").click(parseAddress);
+    $("#linClear").click(function(){
+        $("#linearAddressTextBox").val("");
     });
-    
-
+    $("#phyClear").click(function(){
+        $("#physicalAddressTextBox").val("");
+    });
 });
 
 
 function disableLinearAdress(toDisable){
+    const txt = "Paging disabled. Linear address = Physical address.";
     if (toDisable)
-        $("#linearAddressTextBox").val("Paging disabled. Linear address = Physical address.");
-    else 
-        $("#linearAddressTextBox").val("");
+        $("#linearAddressTextBox").val(txt);
+    else if( $("#linearAddressTextBox").val() === txt)
+    $("#linearAddressTextBox").val("");
     $("#linearAddressTextBox").prop( "disabled", toDisable );
     $("#linearAddrrHex").prop( "disabled", toDisable );
     $("#linearAddrrDec").prop( "disabled", toDisable );
     $("#linearAddrrBin").prop( "disabled", toDisable );
+    $("#linClear").prop( "disabled", toDisable );
 }
 
 function preventType(Base, e) {
+    var basicSet = [48, 49, 96, 97, 8, 35, 36, 37, 38, 39, 40, 46, 16, 20];
     if (Base === BIN) {
-        if (e.keyCode != 48 && e.keyCode != 49 && e.keyCode != 96 && e.keyCode != 97)
+        if (!basicSet.includes(e.keyCode))
             e.preventDefault();
     }
     if (Base === DEC) {
-        if (isNaN(String.fromCharCode(e.which)))
-            event.preventDefault(); 
+        if (!basicSet.includes(e.keyCode) &&
+            !((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)))
+            e.preventDefault(); 
     }
     if (Base === HEX) {
-        if (!((e.keyCode >= 48 && e.keyCode <= 70) || (e.keyCode >= 96 && e.keyCode <= 105)))
-            event.preventDefault(); 
+        if (!basicSet.includes(e.keyCode) && 
+            !((e.keyCode >= 48 && e.keyCode <= 70) || (e.keyCode >= 96 && e.keyCode <= 105)))
+            e.preventDefault(); 
     }
 }
 
 /**
  * convert number base
- * @param {*} n num to convert
+ * @param {*} numStr num to convert
  * @param {*} fromBase 
  * @param {*} toBase 
  */
-function convertNumber(n, fromBase, toBase) {
-    if (n === "") return 0;
-    return parseInt(n.toString(), fromBase).toString(toBase);
+function convertNumber(numStr, fromBase, toBase) {
+    if (numStr === "") return 0;
+    try{
+        var num = bigInt(numStr, fromBase);
+        return num.toString(toBase);
+    }
+    catch(e){
+        return ("Failed to read invalid base("+fromBase+") value.");
+    }
   }
 
 function x32Checked() {
@@ -136,7 +149,7 @@ function x64Checked() {
 }
 
 function pgHandler() {
-    if (this.checked){
+    if ($("#pg").prop("checked")){
         for (var i=0; i<pgSizeSet.elements.length; ++i)
             pgSizeSet.elements[i].disabled = false;
         b4K.checked = true;
@@ -153,4 +166,44 @@ function pgHandler() {
         }
         disableLinearAdress(true);
     }
+}
+
+function parseAddress() {
+    const x32Offset = 4096, x32Table = 1024;
+    const x64_4kOffset = 4096, x64_4kTable = 512;
+    var physicalA, linearA, offset, pt, pd, pdp, pml4;
+    if ($("#physicalAddressTextBox").val() !== "")
+        physicalA = bigInt($("#physicalAddressTextBox").val(), prevPhysicalBase);
+    if ($("#pg").prop("checked"))
+        linearA = bigInt($("#linearAddressTextBox").val(), prevLinearBase);
+    else linearA = physicalA;       // paging disabled. we have only physical addresses.
+    
+
+    if ($("#x32").prop("checked")) {   // x32 mode
+        offset = linearA.mod(x32Offset).toJSNumber();
+        linearA = linearA.divide(x32Offset);
+        pt = linearA.mod(x32Table).toJSNumber();
+        linearA = linearA.divide(x32Table);
+        pd = linearA.mod(x32Table).toJSNumber();
+        var toPrint = "PD["+pd+"]= ?, PT["+pt+"]=?, page offset="+offset+".\n";
+        $("#data").text(toPrint);
+        console.log(toPrint);
+    return;
+    }
+    if ($("#x64").prop("checked")) {   // x64 mode
+        offset = linearA.mod(x64_4kOffset).toJSNumber();
+        linearA = linearA.divide(x64_4kOffset);
+        pt = linearA.mod(x64_4kTable).toJSNumber();
+        linearA = linearA.divide(x64_4kTable);
+        pd = linearA.mod(x64_4kTable).toJSNumber();
+        linearA = linearA.divide(x64_4kTable);
+        pdp = linearA.mod(x64_4kTable).toJSNumber();
+        linearA = linearA.divide(x64_4kTable);
+        pml4 = pd = linearA.mod(x64_4kTable).toJSNumber();
+        var toPrint = "PML4["+pml4+"]=?, PDP["+pdp+"]=?, "+"PD["+pd+"]= ?, PT["+pt+"]=?, page offset="+offset+".\n";
+        $("#data").text(toPrint);
+        console.log(toPrint);
+    return;
+    }
+
 }
